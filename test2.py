e89f7812abd3676c9ffafd5f263644452a2ed237
Matei <matei.florica@ubica-robotics.eu>
2024-03-08 13:45:50 +0100

    Test 2

---
+++ test2.py
@@ -0,0 +1,10 @@
+
+def print_characters_from_string(name):
+    character_list = []
+    for character in name:
+        character_list.append(character)
+    return character_list
+
+name1 = "hjlkhjkhg"
+print(print_characters_from_string(name1))
+
